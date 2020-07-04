4ad81ac001c98335cd3d4bf5d6e9410e8947559b
AmandaDuart <amandaduartdasilva@gmail.com>
2020-07-03 23:20:35 -0300

    primeiro commit

---
+++ json.js
@@ -0,0 +1,27 @@
+let requisicao = new XMLHttpRequest(); // criando o obj que faz a requisição
+requisicao.open("GET", 'https://api.nasa.gov/planetary/apod?api_key=ybrAVis1BdCCRmeIvbCvlIj4vuCIAgcY3d658OkH'); //abrindo req
+
+// TRATAR OS EVENTOS ANTES DE ENVIAR A REQUISIÇÃO !!!
+requisicao.onload = function() { // essa função anonima é callback 
+    if (this.status == 200) {
+
+        let objeto = JSON.parse(requisicao.responseText);
+        console.log(objeto);
+
+        let data = document.getElementById('data');
+        data.textContent = `Data ${objeto.date}`;
+
+        let imagem = document.getElementById('imagem');
+        imagem.src = objeto.url;
+
+        let rodape = document.getElementById('rodape');
+        rodape.textContent = ` Image Credit & ${objeto.copyright}`;
+
+        let descricao = document.getElementById('descricao');
+        descricao.textContent = objeto.explanation;
+
+    }
+
+}
+
+requisicao.send();
